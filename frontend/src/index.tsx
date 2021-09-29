4da1cd84005be5c3d4bc2966413c47017e0591b4
William Hideki Kondo <willkondo@gmail.com>
2021-09-29 18:38:06 -0300

    Install and configure Material-UI

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,12 +1,14 @@
+import { CssBaseline } from '@material-ui/core';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
+import './index.css';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <CssBaseline></CssBaseline>
   </React.StrictMode>,
   document.getElementById('root')
 );
