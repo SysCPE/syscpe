e8201655e94c67660386c13f302493ceb8f8ccf0
Victor Giovannoni <vernalhav@usp.br>
2021-10-05 16:29:10 -0300

    Change indentation to 4 instead of 2

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,15 +1,15 @@
 import koa from 'koa';
 
 const main = () => {
-  const app = new koa();
+    const app = new koa();
 
-  app.use(async (ctx) => {
-    ctx.response.body = 'Hello';
-  });
+    app.use(async (ctx) => {
+        ctx.response.body = 'Hello';
+    });
 
-  app.listen(3000);
+    app.listen(3000);
 
-  console.log('Server started at port 3000');
+    console.log('Server started at port 3000');
 };
 
 main();
