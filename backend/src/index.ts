7b37c4f6c3a2959acc07198d71b5ccdb12d1a3bd
Victor Giovannoni <vernalhav@usp.br>
2021-10-05 17:31:23 -0300

    Add initial database connection

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,20 @@
 import koa from 'koa';
+import { Sequelize } from 'sequelize';
 
-const main = () => {
+const main = async () => {
     const app = new koa();
+    const sequelize = new Sequelize({
+        database: 'postgres',
+        username: 'postgres',
+        password: 'password',
+        host: 'db',
+        port: 5432,
+        dialect: 'postgres'
+    });
+
+    await sequelize.authenticate()
+        .then(() => console.log('Connection has been established successfully.'))
+        .catch((error) => console.error('Unable to connect to the database:', error));        
 
     app.use(async (ctx) => {
         ctx.response.body = 'Hello';
