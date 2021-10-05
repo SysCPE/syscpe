2ea38e78d816e1b0b74ccdeda58e6c40db5b990f
Victor Giovannoni <vernalhav@usp.br>
2021-10-05 17:50:31 -0300

    Change DB connection options object to connection string
    co-authored-by: Giovanni <giovanni@ime.usp.br>
    co-authored-by: João Ramos <joaoramosj@usp.br>
    co-authored-by: Emanuel <enrique.emanuel@usp.br>
    co-authored-by: William <willkondo@usp.br>

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,20 +1,14 @@
 import koa from 'koa';
 import { Sequelize } from 'sequelize';
 
+
 const main = async () => {
     const app = new koa();
-    const sequelize = new Sequelize({
-        database: 'postgres',
-        username: 'postgres',
-        password: 'password',
-        host: 'db',
-        port: 5432,
-        dialect: 'postgres'
-    });
+    const sequelize = new Sequelize(process.env.DB_CONNECTION_STRING as string || '');
 
-    await sequelize.authenticate()
+    sequelize.authenticate()
         .then(() => console.log('Connection has been established successfully.'))
-        .catch((error) => console.error('Unable to connect to the database:', error));        
+        .catch((error) => console.error('Unable to connect to the database:', error));
 
     app.use(async (ctx) => {
         ctx.response.body = 'Hello';
