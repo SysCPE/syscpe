d9de02daacd412fa49c955dad717c9c8239fb7b4
Victor Giovannoni <vernalhav@usp.br>
2021-10-22 17:46:07 -0300

    Add API audience to Auth0Provider component

--- frontend/src/config/auth0.ts
+++ frontend/src/config/auth0.ts
@@ -1,2 +1,4 @@
 export const AUTH0_DOMAIN = process.env.REACT_APP_AUTH0_DOMAIN || '';
 export const AUTH0_CLIENT_ID = process.env.REACT_APP_AUTH0_CLIENT_ID || '';
+export const AUTH0_API_AUDIENCE =
+  process.env.REACT_APP_AUTH0_API_AUDIENCE || 'SysCPE';
--- frontend/src/providers/authentication/providers/AuthenticationAuth0Provider.tsx
+++ frontend/src/providers/authentication/providers/AuthenticationAuth0Provider.tsx
@@ -1,5 +1,9 @@
 import { Auth0Provider, useAuth0 } from '@auth0/auth0-react';
-import { AUTH0_CLIENT_ID, AUTH0_DOMAIN } from 'config/auth0';
+import {
+  AUTH0_API_AUDIENCE,
+  AUTH0_CLIENT_ID,
+  AUTH0_DOMAIN,
+} from 'config/auth0';
 import AuthenticationCore from 'providers/authentication/AuthenticationCore';
 import { FC } from 'react';
 
@@ -8,6 +12,7 @@ const AuthenticationAuth0Provider: FC = ({ children }) => {
     <Auth0Provider
       domain={AUTH0_DOMAIN}
       clientId={AUTH0_CLIENT_ID}
+      audience={AUTH0_API_AUDIENCE}
       redirectUri={window.location.origin}
     >
       <AuthenticationAuth0Core>{children}</AuthenticationAuth0Core>
