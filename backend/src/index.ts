6caa33318bba52124774c98bbe048b7641e477fe
Giovanni Oliveira <giovanni.oliveira@ab-inbev.com>
2021-10-28 17:07:24 -0300

    test2

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -6,7 +6,7 @@ const main = async () => {
   const app = await bootstrap({ CONNECT_TO_DB: false });  
 
   app.listen(SERVER_PORT, () => {
-    console.log(`Server started at port ${SERVER_PORT}.`);
+    console.log(`Server started at port ${SERVER_PORT}...`);
   });
 };
 
