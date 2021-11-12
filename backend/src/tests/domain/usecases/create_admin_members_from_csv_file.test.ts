5c49fdac83800e68bfebde9529829d21b1af1c13
William Hideki Kondo <willkondo@gmail.com>
2021-11-12 16:36:15 -0300

    Parametrize tests

--- backend/src/tests/domain/usecases/create_admin_members_from_csv_file.test.ts
+++ backend/src/tests/domain/usecases/create_admin_members_from_csv_file.test.ts
@@ -1,35 +1,35 @@
 import AdminMemberEntity from 'domain/entities/admin_member_entity';
 import MembersRepository from 'domain/repository/members_repository';
 import CreateAdminMembersFromCSVFile from 'domain/usecases/create_admin_members_from _csv_file';
+import each from 'jest-each';
 import { mock } from 'jest-mock-extended';
 
 describe('CreateAdminMembersFromCSVFiles', () => {
-  it('should run create admin members from csv file usecase', async () => {
-    const mockFile = mock<Blob>();
-    const mockMemberRepository = mock<MembersRepository>();
-    const usecase = new CreateAdminMembersFromCSVFile(mockMemberRepository);
-    const mockAdminMembers = [
-      mock<AdminMemberEntity>(),
-      mock<AdminMemberEntity>(),
-    ];
+  each([[[]], [[mock<AdminMemberEntity>(), mock<AdminMemberEntity>()]]]).it(
+    'should run create admin members from csv file usecase',
+    async (mockAdminMembers: AdminMemberEntity[]) => {
+      const mockFile = mock<Blob>();
+      const mockMemberRepository = mock<MembersRepository>();
+      const usecase = new CreateAdminMembersFromCSVFile(mockMemberRepository);
 
-    mockMemberRepository.readAdminMembersFromCSVFile.mockReturnValue(
-      Promise.resolve(mockAdminMembers)
-    );
+      mockMemberRepository.readAdminMembersFromCSVFile.mockReturnValue(
+        Promise.resolve(mockAdminMembers)
+      );
 
-    await usecase.run(mockFile);
+      await usecase.run(mockFile);
 
-    expect(
-      mockMemberRepository.readAdminMembersFromCSVFile
-    ).toHaveBeenCalledWith(mockFile);
-    expect(mockMemberRepository.saveAdminMember).toHaveBeenCalledTimes(2);
-    expect(mockMemberRepository.saveAdminMember).toHaveBeenNthCalledWith(
-      1,
-      mockAdminMembers[0]
-    );
-    expect(mockMemberRepository.saveAdminMember).toHaveBeenNthCalledWith(
-      2,
-      mockAdminMembers[1]
-    );
-  });
+      expect(
+        mockMemberRepository.readAdminMembersFromCSVFile
+      ).toHaveBeenCalledWith(mockFile);
+      expect(mockMemberRepository.saveAdminMember).toHaveBeenCalledTimes(
+        mockAdminMembers.length
+      );
+      for (let i = 0; i < mockAdminMembers.length; ++i) {
+        expect(mockMemberRepository.saveAdminMember).toHaveBeenNthCalledWith(
+          i + 1,
+          mockAdminMembers[i]
+        );
+      }
+    }
+  );
 });
