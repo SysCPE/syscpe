09de6f6f1027f1a86b8805aedcc5798d0b35b9fa
William Hideki Kondo <willkondo@gmail.com>
2021-11-18 20:02:19 -0300

    Remove migrations runner from bootstrap

--- backend/src/bootstrap.ts
+++ backend/src/bootstrap.ts
@@ -1,48 +1,24 @@
-import koa from 'koa';
-import path from 'path';
-import Umzug from 'umzug';
-import bodyParser from 'koa-body';
-import { Sequelize } from 'sequelize';
-
-import Api from './api';
 import Router from '@koa/router';
+import initModels from 'database';
+import koa from 'koa';
+import Api from './api';
 
+const bootstrap = async () => {
+  initModels();
 
-const bootstrap = async ({ CONNECT_TO_DB = false }) => {
-  const db = CONNECT_TO_DB ? await connectDatabase() : null;  
-  
   const router = new Router();
   router.use(Api.routes()).use(Api.allowedMethods());
 
   const app = new koa();
-  app.use(bodyParser());
+
   app.use(router.routes());
 
-  app.on('error', (err) => { console.log(err) });
-
-  return app;
-};
-
-const connectDatabase = async (): Promise<Sequelize> => {
-  const sequelize = new Sequelize(
-    (process.env.DB_CONNECTION_STRING as string) || ''
-  );
-
-  const umzug = new Umzug({
-    migrations: {
-      path: path.join(__dirname, 'database/migrations'),
-      params: [sequelize.getQueryInterface()],
-    },
-    storage: 'sequelize',
-    storageOptions: {
-      sequelize: sequelize,
-    },
-    logging: console.log,
+  app.on('error', (err) => {
+    // TODO improve this part
+    console.log(err);
   });
 
-  await umzug.up();
-
-  return sequelize;
+  return app;
 };
 
 export default bootstrap;
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,8 @@
 import bootstrap from './bootstrap';
-
 import { SERVER_PORT } from './config/server';
 
 const main = async () => {
-  const app = await bootstrap({ CONNECT_TO_DB: false });  
+  const app = await bootstrap();
 
   app.listen(SERVER_PORT, () => {
     console.log(`Server started at port ${SERVER_PORT}`);
