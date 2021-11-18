3344ad94fefa1cb57ba0547f33bf2de99afe7072
William Hideki Kondo <willkondo@gmail.com>
2021-11-18 20:02:53 -0300

    Implement saveAdminMember in repository

--- backend/src/services/service_members_repository.ts
+++ backend/src/services/service_members_repository.ts
@@ -1,6 +1,11 @@
 import csv from 'csvtojson';
+import { sequelize } from 'database';
+import AdminMember from 'database/models/AdminMember';
+import IdCPE from 'database/models/IdCPE';
+import Member from 'database/models/Member';
 import AdminMemberEntity from 'domain/entities/admin_member_entity';
 import MembersRepository from 'domain/repository/members_repository';
+import { ValidationError } from 'sequelize';
 
 const ServicesMembersRepository: MembersRepository = {
   readAdminMembersFromCSVFile: async (file: Buffer) => {
@@ -8,8 +13,46 @@ const ServicesMembersRepository: MembersRepository = {
 
     return adminMembersJSON.map(__mapAdminMemberJSONToEntity);
   },
-  saveAdminMember: async (adminMember) => {
-    return adminMember;
+  saveAdminMember: async (adminMember: AdminMemberEntity) => {
+    try {
+      return await sequelize.transaction(async (transaction) => {
+        const memberModel = await Member.create(
+          {
+            email: adminMember.email,
+            name: adminMember.name,
+            CPF: adminMember.CPF,
+            RG: adminMember.RG,
+            birthday: adminMember.birthday,
+            gender: adminMember.gender,
+            phone: adminMember.phone,
+            socialName: adminMember.socialName,
+          },
+          { transaction }
+        );
+
+        await AdminMember.create(
+          {
+            memberId: memberModel.id,
+            eachCourse: adminMember.eachCourse,
+            period: adminMember.period,
+            pronoum: adminMember.pronoum,
+            semester: adminMember.semester,
+          },
+          { transaction }
+        );
+
+        await IdCPE.create(
+          { memberId: memberModel.id, idCPE: memberModel.id },
+          { transaction }
+        );
+
+        return adminMember;
+      });
+    } catch (error) {
+      if (error instanceof ValidationError) return null;
+
+      throw error;
+    }
   },
 };
 
