a99626847d80adb1ebb25b1e2aba35cff3809a2e
William Hideki Kondo <willkondo@gmail.com>
2021-11-19 16:34:18 -0300

    Fix LoginButton test

--- frontend/src/components/button/LoginButton.test.tsx
+++ frontend/src/components/button/LoginButton.test.tsx
@@ -1,16 +1,14 @@
-import { useAuth0 } from '@auth0/auth0-react';
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
+import useAuthentication from 'providers/authentication/useAuthentication';
 import LoginButton from './LoginButton';
 
-jest.mock('@auth0/auth0-react', () => ({
-  useAuth0: jest.fn(),
-}));
+jest.mock('providers/authentication/useAuthentication');
 
 describe('LoginButton', () => {
   it('should call loginWithRedirect when login button is clicked', () => {
     const mockLoginWithRedirect = jest.fn();
-    (useAuth0 as jest.Mock).mockReturnValue({
+    (useAuthentication as jest.Mock).mockReturnValue({
       loginWithRedirect: mockLoginWithRedirect,
     });
     render(<LoginButton />);
