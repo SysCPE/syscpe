37855548449ff2969de3c8ac26c9f645d357c871
Victor Giovannoni <vernalhav@usp.br>
2021-11-24 18:18:46 -0300

    refactor upload users route
    co-authored-by: William <willkondo@gmail.br>

--- backend/src/api/index.ts
+++ backend/src/api/index.ts
@@ -1,17 +1,13 @@
-import multer from '@koa/multer';
 import Router from '@koa/router';
 import { checkJwt, getUserInfo } from './middlewares';
-import uploadUsers from './upload_users';
 import MembersRouter from './members';
 
-const upload = multer();
 const router = new Router();
 
 router.get('/private', checkJwt, getUserInfo, (ctx) => {
   ctx.response.body = `Private page: logged in as ${ctx.state.user.email}`;
 });
 
-router.post('/upload-users', upload.single('users'), uploadUsers);
 router.use('/members', MembersRouter.routes());
 
 export default router;
