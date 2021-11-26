1cbc121d0e8bf1e7c608d36ab9156126d811922b
Victor Giovannoni <vernalhav@usp.br>
2021-11-26 11:32:35 -0300

    create optional creationDate attribute in createDepartment

--- backend/src/domain/repository/department_repository.ts
+++ backend/src/domain/repository/department_repository.ts
@@ -1,7 +1,7 @@
 import DepartmentEntity from "domain/entities/department_entity";
 
 type DepartmentRepository = {
-    createDepartment: (name: string) => Promise<DepartmentEntity>;
+    createDepartment: (name: string, creationDate?: Date) => Promise<DepartmentEntity>;
     saveDepartment: (department: DepartmentEntity) => Promise<boolean>;
 };
 
