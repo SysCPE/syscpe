cc09bc5fc93dd9b2abec7dc4073448d4cca9150a
Victor Giovannoni <vernalhav@usp.br>
2021-11-26 15:52:17 -0300

    add belongsTo relationship between AdminMember and Member

--- backend/src/database/index.ts
+++ backend/src/database/index.ts
@@ -1,12 +1,15 @@
 import AdminMember from './models/AdminMember';
+import Department from './models/Department';
 import Member from './models/Member';
 import sequelize from './sequelize';
 
 const initModels = () => {
   Member.initialize(sequelize);
   AdminMember.initialize(sequelize);
+  Department.initialize(sequelize);
 
   Member.hasOne(AdminMember, { foreignKey: 'memberId', as: 'adminMember' });
+  AdminMember.belongsTo(Member, { foreignKey: 'memberId', as: 'member' });
 };
 
 export default initModels;
