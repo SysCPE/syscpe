8d12376ef6071b4173c1c003240104f5f57101ca
oiapokxui <enrique.emanuel@usp.br>
2021-12-01 20:01:49 -0300

    Add script for running unit tests on docker

---
+++ backend/run-unit-tests.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+docker-compose -p syscpe_local_db -f deploy/docker-compose-database.local.yml up -d
+
+docker-compose -p syscpe_local_server -f deploy/docker-compose-unit-tests-runner.yml up
+docker-compose -p syscpe_local_server -f deploy/docker-compose-unit-tests-runner.yml down
+
+docker-compose -p syscpe_local_db -f deploy/docker-compose-database.local.yml down
