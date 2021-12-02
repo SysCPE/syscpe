9c71af3b2508168d193932e223a0b37356a77fe0
Victor Giovannoni <vernalhav@usp.br>
2021-12-02 17:35:24 -0300

    add TDD tests for GET /departments

---
+++ backend/src/tests/integration/get_all_departments.test.ts
@@ -0,0 +1,52 @@
+import bootstrap from "bootstrap";
+import { Server } from "http";
+import request from "supertest";
+import ServicesDepartmentRepository from "services/service_department_repository";
+import { mockDepartments } from "./mocks/mock_departments";
+import DepartmentEntity from "domain/entities/department_entity";
+
+describe('GET /departments', () => {
+    let server: Server;
+    const ROUTE = '/departments';
+
+    beforeAll(async () => {
+        const app = await bootstrap();
+        server = app.listen(4000);
+    });
+    
+    afterAll(() => {
+        server.close();
+    });
+
+    const assertDepartmentInList = (
+        department: DepartmentEntity,
+        departmentList: DepartmentEntity[],
+    ) => {
+        const found = departmentList.find((dept) => department.name === dept.name);
+
+        expect(found).not.toBeNull();
+        expect(found).toMatchObject(department);
+    }
+
+    it('should return all departments', async () => {
+        for (const department of Object.values(mockDepartments)) {
+            await ServicesDepartmentRepository.saveDepartment(
+                department.name,
+                department.creationDate,
+            );
+        }
+
+        const response = await request(server).get(ROUTE);
+        expect(response.status).toBe(200);
+        
+        const parsedDepartments: DepartmentEntity[] = response.body.departments.map(
+            (department: DepartmentEntity) => ({
+                ...department,
+            })
+        );
+
+        for (const department of Object.values(mockDepartments)) {
+            assertDepartmentInList(department, parsedDepartments);
+        }
+    });
+});
