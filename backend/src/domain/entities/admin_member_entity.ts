74e975cdd635c8094f8cb322fef6e867b218fed5
Victor Giovannoni <vernalhav@usp.br>
2021-12-03 16:43:50 -0300

    Add department field to admin member entity

--- backend/src/domain/entities/admin_member_entity.ts
+++ backend/src/domain/entities/admin_member_entity.ts
@@ -1,8 +1,10 @@
+import DepartmentEntity from './department_entity';
 import MemberEntity from './member_entity';
 
 type AdminMemberEntity = {
+  department?: DepartmentEntity;
+  
   pronoun?: string;
-
   eachCourse?: string;
   semester?: number;
   period?: number;
