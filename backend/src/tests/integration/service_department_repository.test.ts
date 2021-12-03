dea4d13bce37f03eb773fb693e5a4e74a489bfee
Victor Giovannoni <vernalhav@usp.br>
2021-12-03 17:50:51 -0300

    add GetDepartment function and tests

---
+++ backend/src/tests/integration/service_department_repository.test.ts
@@ -0,0 +1,28 @@
+import ServicesDepartmentRepository from "services/service_department_repository";
+import { mockDepartments } from "./mocks/mock_departments";
+import useDatabase from "tests/hook/useDatabase";
+
+describe('ServucesDepartmentRepository', () => {
+    useDatabase();
+  
+    beforeEach(async () => {
+        // TODO: create a function to initialize table data?
+        await Promise.all(Object.values(mockDepartments).map(department => {
+            ServicesDepartmentRepository.saveDepartment(
+                department.name,
+                department.creationDate,
+            )
+        }));
+    });
+  
+    it('should find a department', async () => {
+        const result = await ServicesDepartmentRepository.getDepartment('Gestão de Pessoas');
+        expect(result).toBeTruthy();
+        expect(result?.name).toEqual('Gestão de Pessoas');
+    });
+
+    it ('should not find a department', async () => {
+        const result = await ServicesDepartmentRepository.getDepartment('Gestão');
+        expect(result).not.toBeTruthy();
+    });
+});
