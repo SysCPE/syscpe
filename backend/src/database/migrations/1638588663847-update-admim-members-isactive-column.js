e458440b12a42c53848d1562b5d2b557ff4eefa6
oiapokxui <enrique.emanuel@usp.br>
2021-12-04 02:50:22 -0300

    Add admin-members-isactive migration query options

--- backend/src/database/migrations/1638588663847-update-admim-members-isactive-column.js
+++ backend/src/database/migrations/1638588663847-update-admim-members-isactive-column.js
@@ -3,14 +3,20 @@ const Sequelize = require('sequelize');
 module.exports = {
   up: (queryInterface) => {
     return queryInterface.removeColumn('AdminMembers', 'isActive')
-    .then(
-      queryInterface.addColumn('AdminMembers', 'isActive', Sequelize.STRING)
-    )
+    .then( queryInterface.addColumn('AdminMembers', 'isActive', {
+      type: Sequelize.STRING,
+      allowNull: false,
+      defaultValue: "ACTIVE",
+    }))
   },
   down: (queryInterface) => {
     return queryInterface.removeColumn('AdminMembers', 'isActive')
     .then(
-      queryInterface.addColumn('AdminMembers', 'isActive', Sequelize.BOOLEAN)
+      queryInterface.addColumn('AdminMembers', 'isActive', { 
+        type:Sequelize.BOOLEAN,
+        allowNull: false,
+        defaultValue: true,
+      })
     )
   },
 };
