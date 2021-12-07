26eb2bd28284745554869de3a1c85458354fadc1
JVSRamos <joaoramosj@usp.br>
2021-12-07 17:56:23 -0300

    add optional description parameter to saveWorkGroup
    co-authored-by: Victor <vernalhav@usp.br>

--- backend/src/domain/repository/work_group_repository.ts
+++ backend/src/domain/repository/work_group_repository.ts
@@ -3,11 +3,13 @@ import WorkGroupEntity from "domain/entities/work_group_entity";
 type WorkGroupRepository = {
     getWorkGroup: (name: string) => Promise<WorkGroupEntity | null>;
     
-    saveWorkGroup: (name: string, creationDate?: Date) => Promise<WorkGroupEntity>;
+    saveWorkGroup: (name: string, description?: string, creationDate?: Date) => Promise<WorkGroupEntity>;
 
     getAllWorkGroups: () => Promise<WorkGroupEntity[]>;
 
     endWorkGroup: (name: string) => Promise<WorkGroupEntity>;
 };
+
+export class WorkGroupAlreadyExistsError extends Error {};
 
 export default WorkGroupRepository;
