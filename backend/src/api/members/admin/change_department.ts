5b0ec71e57a618f2e8018c0781e8623bf07358f7
Victor Giovannoni <vernalhav@usp.br>
2021-12-06 23:08:12 -0300

    add change_department route

---
+++ backend/src/api/members/admin/change_department.ts
@@ -0,0 +1,22 @@
+import ChangeAdminMemberDepartment, { DepartmentNotFoundError, MemberNotFoundError } from 'domain/usecases/change_admin_member_department';
+import ServicesDepartmentRepository from 'services/service_department_repository';
+import ServicesMembersRepository from 'services/service_members_repository';
+import { Context } from 'koa';
+
+const changeDepartment = async (ctx: Context) => {
+  const usecase = new ChangeAdminMemberDepartment(ServicesMembersRepository, ServicesDepartmentRepository);
+  const memberId = ctx.request.body.memberId;
+  const departmentName = ctx.request.body.departmentName;
+
+  try {
+    await usecase.run(memberId, departmentName);
+  }
+  catch (e) {
+    if (e instanceof MemberNotFoundError)
+      ctx.throw(400, `Cannot find member with ID ${memberId}`);
+    if (e instanceof DepartmentNotFoundError)
+      ctx.throw(400, `Cannot find Department ${departmentName}`);
+  }
+};
+
+export default changeDepartment;
--- backend/src/api/members/admin/index.ts
+++ backend/src/api/members/admin/index.ts
@@ -2,9 +2,11 @@ import Router from '@koa/router';
 import koaBody from 'koa-body';
 import getAllAdminMembers from './get_all_admin_members';
 import uploadUsers from './upload_users';
+import changeDepartment from './change_department';
 
 const AdminRouter = new Router();
 AdminRouter.get('/', getAllAdminMembers);
 AdminRouter.post('/upload-users', koaBody({ multipart: true }), uploadUsers);
+AdminRouter.post('/change-department', koaBody(), changeDepartment);
 
 export default AdminRouter;
