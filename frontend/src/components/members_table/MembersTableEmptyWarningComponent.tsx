b8e63932b8a92d9f457be7c8045316943140cf5b
William Hideki Kondo <willkondo@gmail.com>
2021-12-09 17:08:21 -0300

    Move upload members button to table and refactor table layout

--- frontend/src/components/members_table/MembersTableEmptyWarningComponent.tsx
+++ frontend/src/components/members_table/MembersTableEmptyWarningComponent.tsx
@@ -3,7 +3,9 @@ import { Grid, Typography } from '@material-ui/core';
 const MembersTableEmptyWarningComponent = () => {
   return (
     <Grid container justifyContent="center" sx={{ padding: 1 }}>
-      <Typography variant="body1">Nenhum membro adicionado</Typography>
+      <Typography variant="body1">
+        <b>Nenhum membro adicionado</b>
+      </Typography>
     </Grid>
   );
 };
