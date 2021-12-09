b569431ccda69734816ffe8dc8d4506173e4554e
oiapokxui <enrique.emanuel@usp.br>
2021-12-09 11:45:32 -0300

    Impl. of update department feature

--- backend/src/services/service_department_repository.ts
+++ backend/src/services/service_department_repository.ts
@@ -2,7 +2,8 @@ import AdminMember from "database/models/AdminMember";
 import Department from "database/models/Department";
 import DepartmentEntity from "domain/entities/department_entity";
 import DepartmentRepository from "domain/repository/department_repository";
-import { __mapAdminMemberModelToEntity }  from "services/service_members_repository"
+import { Sequelize, ValidationError } from "sequelize/types";
+import ServicesMembersRepository, { __mapAdminMemberModelToEntity }  from "services/service_members_repository"
 
 const ServicesDepartmentRepository: DepartmentRepository = {
     saveDepartment: async function (name: string, creationDate?: Date): Promise<DepartmentEntity> {
@@ -28,9 +29,29 @@ const ServicesDepartmentRepository: DepartmentRepository = {
 
         if (!result) return null;
         return __mapDepartmentModelToEntity(result);
+    },
+
+    updateDepartment: async function (department: DepartmentEntity) {
+        type returnType = [Boolean, String]
+        const NO_DEPT_FOUND: returnType = [false, "NO DEPT"];
+        const OK : returnType = [true, ""];
+
+        const departmentModel =  await Department.findOne({
+            where: {
+                name:department.name
+            }
+        });
+
+        const returnTuple =  await departmentModel?.update(department)
+        .then(
+            () => OK, 
+            (reason: ValidationError) => [ 
+                false, 
+                reason.errors.map(err => err.message).toString()
+            ] as returnType
+        );
+        return returnTuple || NO_DEPT_FOUND;
     }
-
-
 };
 
 export const __mapDepartmentModelToEntity = (department: Department): DepartmentEntity => {
