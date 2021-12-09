006e5f04387bff1f4a52d41f23452ae1ee35fc51
William Hideki Kondo <willkondo@gmail.com>
2021-12-09 18:06:15 -0300

    Map members data from server to entities

--- frontend/src/services/members_service.ts
+++ frontend/src/services/members_service.ts
@@ -1,13 +1,38 @@
 import axios from 'axios';
 import MemberEntity from 'domain/members/entities/MemberEntity';
+import MemberStatusEntity from 'domain/members/entities/MemberStatusEntity';
+
+type AdminMemberData = {
+  idCPE: number;
+  CPF: string;
+  RG: string;
+  birthday: string;
+  eachCourse: string;
+  name: string;
+  isActive: string;
+};
+
+const _mapStatusDataToEntity = (status: string): MemberStatusEntity => {
+  if (['ACTIVE', 'INACTIVE', 'TIMEOFF'].includes(status))
+    return status as MemberStatusEntity;
+
+  throw new Error(`${status} is not a valid value for MemberStatusEntity`);
+};
 
 const memberService = {
   listMembers: async (): Promise<MemberEntity[]> => {
     const ROUTE = '/members/admin/';
 
     const response = await axios.get(ROUTE);
+    const usersData = response.data.users as AdminMemberData[];
 
-    return [];
+    return usersData.map((userData) => ({
+      idCPE: userData.idCPE.toString(),
+      name: userData.name,
+      course: userData.eachCourse,
+      department: '',
+      status: _mapStatusDataToEntity(userData.isActive),
+    }));
   },
 };
 
