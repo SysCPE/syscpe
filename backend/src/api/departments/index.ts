2b30d7bdfec96307c03b29c771b725f202a1345b
oiapokxui <enrique.emanuel@usp.br>
2021-12-09 11:56:25 -0300

    Create API route for updating a department
    This is not working because the request body always comes empty,
    for some reason i cannot fathom.

--- backend/src/api/departments/index.ts
+++ backend/src/api/departments/index.ts
@@ -2,9 +2,11 @@ import Router from "@koa/router";
 import bodyParser from 'koa-body';
 import createDepartment from "./create_department";
 import getAllDepartments from "./get_all_departments";
+import updateDepartment from "./updateDepartment";
 
 const DepartmentRouter = new Router();
 DepartmentRouter.post('/', bodyParser(), createDepartment);
 DepartmentRouter.get('/', getAllDepartments);
+DepartmentRouter.post('/update-department', updateDepartment);
 
 export default DepartmentRouter;
---
+++ backend/src/api/departments/updateDepartment.ts
@@ -0,0 +1,15 @@
+import ServicesDepartmentRepository from 'services/service_department_repository';
+import { Context } from 'koa';
+import UpdateDepartment from 'domain/usecases/updateDepartment';
+
+const updateDepartment = async (ctx: Context) => {
+  console.log("REQUEST BODY: ");
+  const a = ctx.request.body?.name;  
+  console.log(a);
+  const usecase = new UpdateDepartment(ServicesDepartmentRepository);
+  const response = await usecase.run({name:"ronaldo", creationDate: new Date()});
+
+  ctx.response.body = { response };
+};
+
+export default updateDepartment;
