503c268fa55b9b5dd6f1fa870f7eca3df0dbc249
Emanuel Tavares <enriqueemanuelcontato@gmail.com>
2021-12-09 17:59:24 -0300

    Remove code that belongs to service tests

--- backend/src/tests/integration/update_department.test.ts
+++ backend/src/tests/integration/update_department.test.ts
@@ -4,7 +4,6 @@ import { __mapDepartmentModelToEntity }  from 'services/service_department_repos
 import DepartmentEntity from 'domain/entities/department_entity';
 import { mockDepartments } from './mocks/mock_departments';
 import {  mockAdminMembers } from './mocks/mock_members';
-import Department from 'database/models/Department';
 import useServer from 'tests/hook/useServer';
 import  request  from 'supertest';
 import { Server } from 'http';
@@ -35,21 +34,6 @@ describe('Update Department route: /departments/update-department', () => {
     beforeEach(async () => {
         await populateDB(); 
     });
-
-    const assertDepartmentExists = async (expectedDepartment: DepartmentEntity) => {
-        const acquiredDepartmentModel = (await Department.findOne({
-            where: {
-                name:expectedDepartment.name,
-            },
-        }))!;   //This is ugly
-
-        const acquiredDepartment = __mapDepartmentModelToEntity(acquiredDepartmentModel);
-        expect(expectedDepartment).not.toBeNull();
-        expect(acquiredDepartment.name).toBe(expectedDepartment.name);
-        // expect(acquiredDepartment.creationDate).toBe(expectedDepartment.creationDate);
-        expect(acquiredDepartment.directorId).toBe(expectedDepartment.directorId);
-        expect(acquiredDepartment.viceDirectorId).toBe(expectedDepartment.viceDirectorId);
-    };
 
     it('should return bad request when body is empty', async () => {
         const response = await request(server).post(ROUTE);
@@ -90,28 +74,4 @@ describe('Update Department route: /departments/update-department', () => {
         expect(response.text).toBe("NO ADMIN MEMBER FOUND WITH ID " + department.viceDirectorId);
     });
 
-    it('should update department given that director and vice director are NOT present', async () => {
-        const gestaoDePessoasUpdate: DepartmentEntity = { 
-            name: inovaTec.name,
-            creationDate: inovaTec.creationDate,
-        };
-        const response = await request(server).post(ROUTE).send(gestaoDePessoasUpdate);
-
-        expect(response.statusCode).toBe(200);
-        await assertDepartmentExists(gestaoDePessoasUpdate);
-    });
-
-    it('should update department given that director and vice director are present', async () => {
-        const inovaTecUpdate: DepartmentEntity = { 
-            name: inovaTec.name,
-            creationDate: inovaTec.creationDate,
-            directorId:mockAdminMembers['a@gmail.com'].idCPE,
-            viceDirectorId:mockAdminMembers['b@gmail.com'].idCPE,
-        };
-        const response = await request(server).post(ROUTE).send(inovaTecUpdate);
-
-
-        expect(response.statusCode).toBe(200);
-        await assertDepartmentExists(inovaTecUpdate);
-    });
 })
