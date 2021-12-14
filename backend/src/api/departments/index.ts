df7db805389692a89da5ada83c54e412dab32011
Victor Giovannoni <vernalhav@usp.br>
2021-12-14 18:05:07 -0300

    Add delete department route

--- backend/src/api/departments/index.ts
+++ backend/src/api/departments/index.ts
@@ -1,6 +1,7 @@
 import Router from "@koa/router";
 import bodyParser from 'koa-body';
 import createDepartment from "./create_department";
+import deleteDepartment from "./delete_department";
 import getAllDepartments from "./get_all_departments";
 import updateDepartment from "./update_department";
 
@@ -8,5 +9,6 @@ const DepartmentRouter = new Router();
 DepartmentRouter.post('/', bodyParser(), createDepartment);
 DepartmentRouter.get('/', getAllDepartments);
 DepartmentRouter.post('/update-department', bodyParser(), updateDepartment);
+DepartmentRouter.post('/delete', bodyParser(), deleteDepartment);
 
 export default DepartmentRouter;
